7743780808f9c4581a1381e41cf0f7d7761b9a41
Thomas Tortorini <thomastortorini@gmail.com>
2020-08-30 22:42:28 +0200

    gsSynth: handle new gsuiSynthesizer usage + synth's env

--- gsSynth/gsSynth.js
+++ gsSynth/gsSynth.js
@@ -2,32 +2,71 @@
 
 class GSSynth {
 	constructor() {
-		const uiSynth = new gsuiSynthesizer(),
-			uiLFO = new gsuiLFO(),
+		const uiSynth = document.createElement( "gsui-synthesizer" ),
+			uiEnv = document.createElement( "gsui-envelope" ),
+			uiLFO = document.createElement( "gsui-lfo" ),
 			dataSynth = new DAWCore.controllers.synth( {
 				dataCallbacks: {
 					addOsc: ( id, osc ) => uiSynth.addOscillator( id, osc ),
 					removeOsc: id => uiSynth.removeOscillator( id ),
-					changeOscProp: ( id, k, v ) => uiSynth.getOscillator( id ).change( k, v ),
-					updateOscWave: id => uiSynth.getOscillator( id ).updateWave(),
-					changeLFOProp: ( k, v ) => uiLFO.change( k, v ),
+					changeEnvProp: ( k, v ) => uiEnv.setAttribute( k, v ),
+					changeLFOProp: ( k, v ) => uiLFO.setAttribute( k, v ),
+					changeOscProp: ( id, k, v ) => uiSynth.getOscillator( id ).setAttribute( k, v ),
+					updateEnvWave: () => uiEnv.updateWave(),
 					updateLFOWave: () => uiLFO.updateWave(),
+					updateOscWave: id => uiSynth.getOscillator( id ).updateWave(),
 				},
 			} );
 
-		this.rootElement = uiSynth.rootElement;
+		this.rootElement = uiSynth;
+		this._uiEnv = uiEnv;
 		this._uiLFO = uiLFO;
-		this._uiSynth = uiSynth;
 		this._dataSynth = dataSynth;
 		this._dawcore =
 		this._synthId = null;
 		Object.seal( this );
 
-		uiSynth.rootElement.querySelector( ".gsuiSynthesizer-lfo" ).append( uiLFO.rootElement );
-		uiLFO.oninput = this._oninputLFO.bind( this );
-		uiSynth.oninput = this._oninputSynth.bind( this );
-		uiLFO.onchange =
-		uiSynth.onchange = this._onchange.bind( this );
+		uiSynth.setEnvelope( uiEnv );
+		uiSynth.setLFO( uiLFO );
+		uiSynth.addEventListener( "gsuiEvents", e => {
+			const d = e.detail,
+				a = d.args,
+				id = this._synthId,
+				dc = this._dawcore;
+
+			switch ( d.component ) {
+				case "gsuiEnvelope":
+					switch ( d.eventName ) {
+						case "toggle": dc.callAction( "toggleEnv", id ); break;
+						case "change": dc.callAction( "changeEnv", id, ...a ); break;
+						case "liveChange": dc.liveChangeSynth( id, { env: { [ a[ 0 ] ]: a[ 1 ] } } ); break;
+					}
+					break;
+				case "gsuiLFO":
+					switch ( d.eventName ) {
+						case "toggle": dc.callAction( "toggleLFO", id ); break;
+						case "change": dc.callAction( "changeLFO", id, ...a ); break;
+						case "liveChange": dc.liveChangeSynth( id, { lfo: { [ a[ 0 ] ]: a[ 1 ] } } ); break;
+					}
+					break;
+				case "gsuiSynthesizer":
+					switch ( d.eventName ) {
+						case "addOscillator": dc.callAction( "addOscillator", id ); break;
+						case "reorderOscillator": dc.callAction( "reorderOscillator", id, a[ 0 ] ); break;
+					}
+					break;
+				case "gsuiOscillator": {
+					const oscId = e.target.dataset.id;
+
+					switch ( d.eventName ) {
+						case "remove": dc.callAction( "removeOscillator", id, oscId ); break;
+						case "change": dc.callAction( "changeOscillator", id, oscId, ...a ); break;
+						case "liveChange": dc.liveChangeSynth( id, { oscillators: { [ oscId ]: { [ a[ 0 ] ]: a[ 1 ] } } } ); break;
+					}
+				} break;
+			}
+			e.stopPropagation();
+		} );
 	}
 
 	// .........................................................................
@@ -35,7 +74,7 @@ class GSSynth {
 		this._dawcore = core;
 	}
 	setWaveList( arr ) {
-		this._uiSynth.setWaveList( arr );
+		this.rootElement.setWaveList( arr );
 	}
 	selectSynth( id ) {
 		if ( id !== this._synthId ) {
@@ -51,46 +90,21 @@ class GSSynth {
 			get = this._dawcore.get;
 
 		if ( "beatsPerMeasure" in obj || "stepsPerBeat" in obj ) {
+			this._uiEnv.timeSignature( get.beatsPerMeasure(), get.stepsPerBeat() );
 			this._uiLFO.timeSignature( get.beatsPerMeasure(), get.stepsPerBeat() );
 		}
 		if ( synObj ) {
 			this._dataSynth.change( synObj );
 			if ( synObj.oscillators ) {
-				this._uiSynth.reorderOscillators( synObj.oscillators );
+				this.rootElement.reorderOscillators( synObj.oscillators );
 			}
 		}
 		if ( "synthOpened" in obj ) {
 			this.selectSynth( obj.synthOpened );
 		}
 	}
-	resize() {
-		this._uiLFO.resize();
-	}
-	resizing() {
-		this._uiLFO.resizing();
-	}
-	attached() {
-		this._uiLFO.attached();
-		this._uiSynth.attached();
-	}
 	clear() {
 		this._dataSynth.clear();
-	}
-
-	// events:
-	// .........................................................................
-	_onchange( act, ...args ) {
-		this._dawcore.callAction( act, this._synthId, ...args );
-	}
-	_oninputSynth( id, prop, val ) {
-		const oscillators = { [ id ]: { [ prop ]: val } };
-
-		this._dawcore.liveChangeSynth( this._synthId, { oscillators } );
-	}
-	_oninputLFO( prop, val ) {
-		const lfo = { [ prop ]: val };
-
-		this._dawcore.liveChangeSynth( this._synthId, { lfo } );
 	}
 }
 
