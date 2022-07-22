6cf760f3ed20013cf271ef8699368fd22eb31d58
mr21 <thomastortorini@gmail.com>
2022-07-22 17:32:48 +0200

    gsLibrary: #loadSample + #playSample

--- gsLibrary/gsLibrary.js
+++ gsLibrary/gsLibrary.js
@@ -3,9 +3,16 @@
 class GSLibrary {
 	rootElement = null
 	#dawcore = null;
+	#buffers = new Map();
 
 	constructor() {
 		this.rootElement = GSUI.$createElement( "gsui-library" );
+		GSUI.$listenEvents( this.rootElement, {
+			gsuiLibrary: {
+				loadSample: d => this.#loadSample( d.args[ 0 ] ),
+				playSample: d => this.#playSample( d.args[ 0 ] ),
+			},
+		} );
 	}
 
 	// .........................................................................
@@ -18,6 +25,30 @@ class GSLibrary {
 		} );
 	}
 	clear() {
+	}
+
+	// .........................................................................
+	#loadSample( id ) {
+		this.rootElement.loadSample( id );
+		fetch( `/samples/${ id }.wav` )
+			.then( res => res.arrayBuffer() )
+			.then( arr => this.#dawcore.$getCtx().decodeAudioData( arr ) )
+			.then( buffer => {
+				this.#buffers.set( id, Object.seal( { buffer, absn: null } ) );
+				this.rootElement.readySample( id );
+				this.#playSample( id );
+			} );
+	}
+	#playSample( id ) {
+		const obj = this.#buffers.get( id );
+		const ctx = this.#dawcore.$getCtx();
+		const absn = ctx.createBufferSource();
+
+		obj.absn = absn;
+		absn.buffer = obj.buffer;
+		absn.connect( this.#dawcore.$getAudioDestination() );
+		absn.start();
+		this.rootElement.playSample( id, obj.buffer.duration );
 	}
 }
 
