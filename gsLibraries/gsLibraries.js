fa57cdd2f5785d97c81bbbe6217763f81a7e601a
mr21 <thomastortorini@gmail.com>
2022-09-02 22:10:02 +0200

    gsLibraries: remove the ABSN code from here

--- gsLibraries/gsLibraries.js
+++ gsLibraries/gsLibraries.js
@@ -3,11 +3,6 @@
 class GSLibraries {
 	rootElement = null
 	#dawcore = null;
-	#absn = null;
-	#buffers = {
-		default: new Map(),
-		local: new Map(),
-	};
 
 	constructor() {
 		this.rootElement = GSUI.$createElement( "gsui-libraries" );
@@ -24,6 +19,10 @@ class GSLibraries {
 				playSample: ( d, tar ) => {
 					this.#playSample( GSUI.$getAttribute( tar, "name" ), d.args[ 0 ] );
 				},
+				stopSample: () => {
+					this.#dawcore.$buffersStopBuffer();
+					this.stop();
+				},
 			},
 		} );
 	}
@@ -37,38 +36,22 @@ class GSLibraries {
 			this.rootElement.getLibrary( "default" ).setLibrary( gsuiLibrarySamples );
 		} );
 	}
-	addLocalSamples( arr ) {
-		if ( arr.length > 0 ) {
-			arr.forEach( smp => {
-				if ( Array.isArray( smp ) ) {
-					this.#buffers.local.set( smp[ 0 ], Object.seal( { buffer: smp[ 1 ] } ) );
-				}
-			} );
-			this.rootElement.getLibrary( "local" ).setLibrary( arr.map( smp =>
+	addLocalSamples( files ) {
+		if ( files.length > 0 ) {
+			this.rootElement.getLibrary( "local" ).setLibrary( files.map( smp =>
 				Array.isArray( smp )
-					? [ smp[ 0 ], smp[ 2 ], smp[ 3 ] ]
+					? [ smp[ 0 ], gsuiWaveform.getPointsFromBuffer( 40, 10, smp[ 1 ] ), smp[ 2 ] ]
 					: smp
 			) );
 			GSUI.$setAttribute( this.rootElement, "lib", "local" );
 		}
 	}
-	getSample( lib, idStr ) {
-		const [ id, name ] = idStr.split( ":" );
-		const buf = this.#buffers[ lib ].get( id );
-
-		return buf
-			? Promise.resolve( buf.buffer )
-			: this.#loadSample( lib, id );
-	}
 	stop() {
-		this.#absn?.stop();
 		this.rootElement.getLibrary( "local" ).stopSample();
 		this.rootElement.getLibrary( "default" ).stopSample();
 	}
 	clear() {
 		this.stop();
-		this.#buffers.default.clear();
-		this.#buffers.local.clear();
 		this.rootElement.getLibrary( "local" ).clear();
 	}
 
@@ -80,27 +63,17 @@ class GSLibraries {
 				return Promise.resolve();
 			case "default":
 				this.rootElement.getLibrary( "default" ).loadSample( id );
-				return fetch( `/🥁/${ id }.wav` )
-					.then( res => res.arrayBuffer() )
-					.then( arr => this.#dawcore.$getCtx().decodeAudioData( arr ) )
-					.then( buffer => {
-						this.#buffers.default.set( id, Object.seal( { buffer } ) );
+				return this.#dawcore.$buffersLoadURLBuffer( id )
+					.then( buf => {
 						this.rootElement.getLibrary( "default" ).readySample( id );
-						return buffer;
+						return buf;
 					} );
 		}
 	}
 	#playSample( lib, id ) {
-		const obj = this.#buffers[ lib ].get( id );
-		const ctx = this.#dawcore.$getCtx();
-		const absn = ctx.createBufferSource();
+		const buf = this.#dawcore.$buffersPlayBuffer( id );
 
-		this.#absn?.stop();
-		this.#absn = absn;
-		absn.buffer = obj.buffer;
-		absn.connect( this.#dawcore.$getAudioDestination() );
-		absn.start();
-		this.rootElement.getLibrary( lib ).playSample( id, obj.buffer.duration );
+		this.rootElement.getLibrary( lib ).playSample( id, buf.duration );
 	}
 }
 
